16e57cc4c8af7a9430df804cdc8cb8cb25ee8e19
vloison <virg.loison@gmail.com>
2025-07-03 19:15:47 +0200

    delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name="fadin",
-    version="0.0.1",
-    description="FaDIn: Fast Discretized Inference for Hawkes Processes with General Parametric Kernels",
-    packages=setuptools.find_packages(),
-    python_requires='>=3.7',
-)
